8a4f8bbd8a37acfb7ca5db206bbde25361b47b76
fxxkscript <ray@tomo.im>
2019-03-31 13:03:18 +0800

    build

--- lib/screens/friends/fans.dart
+++ lib/screens/friends/fans.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wshop/api/friends.dart';
 import 'package:wshop/components/FollowBtn.dart';
-import 'package:wshop/models/auth.dart';
 import 'package:wshop/models/author.dart';
 
 class FansScreen extends StatefulWidget {
@@ -36,73 +35,13 @@ class FansScreenState extends State<FansScreen> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
-          middle: Text('关注'),
+          middle: Text('我的粉丝'),
         ),
         child: RefreshIndicator(
             displacement: 80,
             child: ListView.builder(
                 itemCount: count,
                 itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return Container(
-                      decoration: BoxDecoration(color: Colors.white),
-                      margin: EdgeInsets.only(top: 10, bottom: 10),
-                      padding: EdgeInsets.only(
-                          top: 8, bottom: 8, left: 16, right: 16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(right: 10),
-                            child: ClipRRect(
-                              child: Container(
-                                width: 56,
-                                height: 56,
-                                decoration: BoxDecoration(
-                                    color: Color.fromARGB(255, 248, 248, 248)),
-                                child: Image.network(
-                                  Auth().avatar,
-                                  width: 56,
-                                  height: 56,
-                                ),
-                              ),
-                              borderRadius: BorderRadius.circular(28),
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  '我的粉丝',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .caption
-                                      .copyWith(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500),
-                                ),
-                                Text(
-                                  '1280',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subtitle
-                                      .copyWith(fontSize: 12),
-                                )
-                              ],
-                            ),
-                          ),
-                          Icon(
-                            Icons.chevron_right,
-                            color: Color.fromARGB(255, 209, 209, 214),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-
-                  index = index - 1;
-
                   return Container(
                     decoration: BoxDecoration(color: Colors.white),
                     padding:
