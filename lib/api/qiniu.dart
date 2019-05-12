2d16fcd5dc857f8cc062a8ed96bfb362d0887edf
fxxkscript <ray@tomo.im>
2019-05-12 16:31:19 +0800

    上传图片改成http client 直接传

--- lib/api/qiniu.dart
+++ lib/api/qiniu.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
-import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wshop/models/auth.dart';
+import 'package:uuid/uuid.dart';
 import 'package:wshop/utils/http_client.dart';
 
 const channel = const MethodChannel('com.meizizi.doraemon/door');
+Uuid uuid = new Uuid();
+Dio dio = new Dio(BaseOptions(baseUrl: 'http://upload.qiniup.com'));
 
 class Qiniu {
   static Future<String> getToken({
@@ -21,12 +23,33 @@ class Qiniu {
 
   static Future<String> upload(
       BuildContext context, Uint8List data, String token) async {
-    String key = DateTime.now().millisecondsSinceEpoch.toString() +
-        Random(Auth().uid).nextInt(100000).toString() +
-        '.jpg';
+    String key = uuid.v1() + '.jpg';
 
-    await channel.invokeMethod('upload',
-        <String, dynamic>{'imageData': data, 'token': token, 'key': key});
+    FormData formData = new FormData.from({
+      'token': token,
+      'key': key,
+      'file': new UploadFileInfo.fromBytes(data, key),
+    });
+
+//    await channel.invokeMethod('upload',
+//        <String, dynamic>{'imageData': data, 'token': token, 'key': key});
+    try {
+      await dio.post('/', data: formData,
+          onSendProgress: (int sent, int total) {
+        print("$sent $total");
+      });
+    } on DioError catch (e) {
+      if (e.response is Response) {
+        print(e.response.data);
+        print(e.response.headers);
+        print(e.response.request);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        print(e.request);
+        print(e.message);
+      }
+      throw e;
+    }
 
     return key;
   }
