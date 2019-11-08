db3ec34f46be244aa99865f6336cb68defa20948
fxxkscript <ray@tomo.im>
2019-11-08 10:50:36 +0800

    fix: request permission for android

--- android/app/src/main/java/com/meizizi/doraemon/MainActivity.java
+++ android/app/src/main/java/com/meizizi/doraemon/MainActivity.java
@@ -1,7 +1,6 @@
 package com.meizizi.doraemon;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -22,8 +21,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends FlutterActivity {
+    private static String[] PERMISSIONS_STORAGE = {
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
     private static final String CHANNEL = "com.meizizi.doraemon/door";
+    private static final int REQUEST_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,14 @@ public class MainActivity extends FlutterActivity {
                     share(call.arguments);
                 }
             });
+
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
+
     }
 
     @SuppressWarnings("unchecked")
